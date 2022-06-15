89f4e34cef94ea0d44ee19f34183de1051cf194f
mGurau <marian.gurau123@gmail.com>
2022-06-15 23:35:45 +0300

    Added latest code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'omarmhammouda';
+  title = 'omarmhammouda'; 
+  clientHeight: number;
+
+constructor() {
+   this.clientHeight = window.innerHeight; 
+}
 }
